2dc81c45cf1bfd5b7e490482310e35f355411867
akutoyuuki <akutoyuuki@gmail.com>
2018-08-21 18:28:28 +0900

    日本語化及びドットインストール完了

--- myblog/app/models/post.rb
+++ myblog/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
+    # @post.comment
+    validates :title, :presence => {:message => '必須項目です'}, length: {minimum: 3, message: 'タイトルが短すぎます'}
+    validates :body, :presence => {:message => '必須項目です'}
 end
